359c79229282db07abd978a65c2c1b3bcec8178e
Marcos Bento <marcosKyllenor@gmail.com>
2019-08-29 21:50:16 -0300

    validando numeros

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -8,15 +8,22 @@ public class Loteria {
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 numeros");
         for (int i = 0; i < 6; i++) {
-            capNum(in, i);
+            int num = -1;
+            while (num == -1) {
+                num = capNum(in, i);
 
+            }
         }
     }
 
     public static int capNum(Scanner in, int i) {
-        System.out.println("Diginte o " + (i+1) + "º numero");
-        return 0;
-
+        System.out.println("Diginte o " + (i + 1) + "º numero");
+        int num = in.nextInt();
+        if (num >= 1 && num <= 60) {
+            return num;
+        } else {
+            return -1;
+        }
     }
 
 }
